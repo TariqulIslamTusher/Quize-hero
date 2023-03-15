fc468ce5603ff77b7f99a147e5d4d90c525854fd
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-03-15 23:13:25 +0600

    corected the eventListener spelling in submit btn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const displayQuiz = (data) => {
   }
 
 
-  data.forEach((quiz, i) => {
+  data.forEach((quiz, ) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -76,6 +76,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
+
 document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
